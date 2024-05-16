72c08f370a1c94b506e2df772771453d2bad0988
flavokrkkk <145288995+flavokrkkk@users.noreply.github.com>
2024-05-16 09:55:50 +0300

    remove pdf parser

--- client/src/pages/BasketPage/BasketPage.tsx
+++ client/src/pages/BasketPage/BasketPage.tsx
@@ -1,26 +1,7 @@
-import { useEffect, useState } from "react";
-import pdf from "./parse.pdf";
-import pdfToText from "react-pdftotext";
-
 const BasketPage = () => {
-  const [datas, setData] = useState<string[]>([]);
-
-  const extractText = async () => {
-    const file = await fetch(pdf)
-      .then((res) => res.blob())
-      .catch((err) => console.log(err));
-    pdfToText(file).then((text) => setData([...datas, text]));
-  };
-
-  useEffect(() => {
-    extractText();
-  }, []);
-
   return (
     <div>
-      {datas.map((el) => (
-        <div>{el}</div>
-      ))}
+      <h1>Basket</h1>
     </div>
   );
 };
